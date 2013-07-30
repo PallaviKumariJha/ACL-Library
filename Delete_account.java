8dc60812c866305675d7128483573a67dcf4e08b
PallaviKumariJha <pallavikumarijha@gmail.com>
2013-07-30 11:43:46 +0530

    Create Delete_account.java

---
+++ Delete_account.java
@@ -0,0 +1,117 @@
+package application;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import connectivity.Connect;
+
+
+public class deleteaccount extends HttpServlet {
+
+  /**
+	 * Constructor of the object.
+	 */
+	public deleteaccount() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		int retval=0;
+		Statement stmt = null;
+		try {
+			stmt = Connect.connct();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		String query="delete from users where uid=";
+		try {
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+			retval=stmt.executeUpdate(query);
+			if (retval>0){
+				out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+				out.println("<HTML>");
+				out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+				out.println("  <BODY>");
+				out.print(" Your Account has been successfully Deleted ");
+				out.print(this.getClass());
+				out.println("  </BODY>");
+				out.println("</HTML>");
+				out.flush();
+				out.close();
+			}
+			else
+			{
+				out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+				out.println("<HTML>");
+				out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+				out.println("  <BODY>");
+				out.print(" Your Account has not been Deleted ");
+				out.print(this.getClass());
+				out.println("  </BODY>");
+				out.println("</HTML>");
+				out.flush();
+				out.close();	
+			}
+				
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+		out.println("<HTML>");
+		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+		out.println("  <BODY>");
+		out.print(" Your Account is Deleted ");
+		out.print(this.getClass());
+		out.println("  </BODY>");
+		out.println("</HTML>");
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
